3644305a3ea47882de08d4ba40c850eb5f2f1a2a
xzz <1805402615@qq.com>
2021-03-31 22:24:39 +0800

    first add

---
+++ ASSIGNMENT/Assignment1/代码框架/build/CMakeFiles/Rasterizer.dir/DependInfo.cmake
@@ -0,0 +1,43 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/xzz/GAMES101/ASSIGNMENT/Assignment1/代码框架/Triangle.cpp" "/home/xzz/GAMES101/ASSIGNMENT/Assignment1/代码框架/build/CMakeFiles/Rasterizer.dir/Triangle.cpp.o"
+  "/home/xzz/GAMES101/ASSIGNMENT/Assignment1/代码框架/main.cpp" "/home/xzz/GAMES101/ASSIGNMENT/Assignment1/代码框架/build/CMakeFiles/Rasterizer.dir/main.cpp.o"
+  "/home/xzz/GAMES101/ASSIGNMENT/Assignment1/代码框架/rasterizer.cpp" "/home/xzz/GAMES101/ASSIGNMENT/Assignment1/代码框架/build/CMakeFiles/Rasterizer.dir/rasterizer.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/xzz/opencv-3.4.13/build"
+  "/home/xzz/opencv-3.4.13/include"
+  "/home/xzz/opencv-3.4.13/include/opencv"
+  "/home/xzz/opencv-3.4.13/modules/core/include"
+  "/home/xzz/opencv-3.4.13/modules/flann/include"
+  "/home/xzz/opencv-3.4.13/modules/imgproc/include"
+  "/home/xzz/opencv-3.4.13/modules/ml/include"
+  "/home/xzz/opencv-3.4.13/modules/photo/include"
+  "/home/xzz/opencv-3.4.13/modules/video/include"
+  "/home/xzz/opencv-3.4.13/modules/dnn/include"
+  "/home/xzz/opencv-3.4.13/modules/features2d/include"
+  "/home/xzz/opencv-3.4.13/modules/imgcodecs/include"
+  "/home/xzz/opencv-3.4.13/modules/shape/include"
+  "/home/xzz/opencv-3.4.13/modules/videoio/include"
+  "/home/xzz/opencv-3.4.13/modules/calib3d/include"
+  "/home/xzz/opencv-3.4.13/modules/highgui/include"
+  "/home/xzz/opencv-3.4.13/modules/objdetect/include"
+  "/home/xzz/opencv-3.4.13/modules/stitching/include"
+  "/home/xzz/opencv-3.4.13/modules/superres/include"
+  "/home/xzz/opencv-3.4.13/modules/ts/include"
+  "/home/xzz/opencv-3.4.13/modules/videostab/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
